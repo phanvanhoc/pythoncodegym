3622db879c6ce4d88d35ffb884769427dfb7ed44
phanvanhoc <144450095+phanvanhoc@users.noreply.github.com>
2023-09-21 15:03:18 +0700

    Create kiem_tra_float_chan_le.py

---
+++ Thực hành tuần 3/kiem_tra_float_chan_le.py
@@ -0,0 +1,7 @@
+n = float(input("nhập số biết đó là số chăn hay lẽ :"))
+if n % 2 == 0:
+    print(f"số {n} là số chẵn")
+elif n % 2 == 1:
+    print(f"số {n} là số lẽ")
+else:
+    print(f"bạn nhập {n} có lẽ đã sai thông tin")
