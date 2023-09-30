215b1efd6d9974b5a67accf8f016ef3e207ddd17
phanvanhoc <144450095+phanvanhoc@users.noreply.github.com>
2023-09-30 14:03:20 +0700

    Create bangcuuchuong.py

---
+++ Thực hành tuần 4/bangcuuchuong.py
@@ -0,0 +1,5 @@
+for i in range(1, 10):
+    for j in range(1, 11):
+        print(f"bảng cửa chương là {i} x {j} = {i*j}",)
+        print()
+        print("-------------------------------------------")  
